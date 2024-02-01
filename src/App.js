11aed83baccf8f5df68448875a78d4f311adaebb
TiagoJBO <142507640+TiagoJBO@users.noreply.github.com>
2024-02-01 00:41:09 -0300

    Projeto-react  estilizado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import Avatar from './assets/avatar.png';
+import Arrow from './assets/arrow.png';
 
-import { Container, H1 } from "./styles";
+import { Container, Image, ConteinerItens, H1, InputLabel, Input, Button, } from "./styles";
 
 const App = () => {
 
-  const hello = "Hello React"
+
   return (
     <Container>
-      <H1>{hello}</H1>
-    </Container>
+      <Image alt="logo-image" src={Avatar} />
+      <ConteinerItens>
+        <H1>Olá!</H1>
+        <InputLabel>Nome</InputLabel>
+        <Input placeholder='Nome' />
+
+        <InputLabel>Idade</InputLabel>
+        <Input placeholder='Idade' />
+
+        <Button>Cadastrar <img alt='seta' src={Arrow}></img></Button>
+      </ConteinerItens>
+    </Container >
   );
 
 }
