4c52cb7e10cc3e2232e0135f9448e641ad87e66f
TiagoJBO <142507640+TiagoJBO@users.noreply.github.com>
2024-02-03 00:36:20 -0300

    Atualizado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Avatar from './assets/avatar.png';
 import Arrow from './assets/arrow.png';
+import Trash from './assets/trash.png'
 
-import { Container, Image, ConteinerItens, H1, InputLabel, Input, Button, } from "./styles";
+import { Container, Image, ConteinerItens, H1, InputLabel, Input, Button, User, } from "./styles";
 
 const App = () => {
+  const users = [
+    { id: Math.random(), name: "Tiago", age: 40, },
+    { id: Math.random(), name: "Joana", age: 7, }
+  ];
 
 
   return (
@@ -18,7 +23,23 @@ const App = () => {
         <InputLabel>Idade</InputLabel>
         <Input placeholder='Idade' />
 
-        <Button>Cadastrar <img alt='seta' src={Arrow}></img></Button>
+        <Button>
+          Cadastrar <img alt='seta' src={Arrow} />
+        </Button>
+
+        <ul>
+
+          {users.map((user) => (
+
+            <User key={user.id}>
+              <p>{user.name}</p><p>{user.age}</p>
+              <button><img src={Trash} alt='lata-de-lixo' /></button>
+            </User>
+          ))}
+
+
+
+        </ul>
       </ConteinerItens>
     </Container >
   );
--- src/styles.js
+++ src/styles.js
@@ -111,10 +111,43 @@ export const Button = styled.button`
     &:active{
         opacity:0.5;
     }
-
-
-
-
-
-    
+        
 `;
+export const User = styled.li`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    box-shadow: 0px 4px 4px 0px #00000040;
+    background: #FFFFFF40;
+    width: 342px;
+    height: 58px;
+    top: 193px;
+    left: 36px;
+    padding: 15px, 20px, 15px, 25px;
+    border-radius: 14px;
+    margin-top: 20px;
+
+    padding-left:25px;
+
+
+    border: none;
+    outline: none;
+
+    p{
+        font-style: normal;
+        font-weight: normal;
+        font-size: 20px;
+        line-height: 28px;
+        color: #FFFFFF;
+
+    }
+
+    button{
+        background: none;
+        border: none;
+        cursor: pointer;
+
+    }
+   
+
+`
