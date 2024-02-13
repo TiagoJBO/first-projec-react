39e9b795af78131506a45ea14f2f5c4d9ce342ae
TiagoJBO <timarao2@hotmail.com>
2024-02-12 22:56:12 -0300

    Organizando pasta

--- src/Containers/Styles/index.js
+++ src/Containers/Styles/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import GlobalStyle from "./globalStyle";
-import Routes from "./routes";
+import Routes from "../../routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
